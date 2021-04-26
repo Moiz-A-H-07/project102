75f266a6c22d5ab8f2097865dd162bb8a3947c3a
Moiz-A-H-07 <70146888+Moiz-A-H-07@users.noreply.github.com>
2021-04-26 16:32:37 +0530

    Add files via upload

---
+++ project102.py
@@ -0,0 +1,11 @@
+import cv2
+import dropbox
+import time
+import random
+starttime=time.time()
+
+def main():
+    while(True):
+        if((time.time()-starttime)>=300):
+            print("5 minutes done")
+main()
